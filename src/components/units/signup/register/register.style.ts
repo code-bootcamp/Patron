cb307c2e2d55b46c380e7c1b698055b294aa896b
ekiankim92 <eunkukkim1992@gmail.com>
2022-01-01 12:52:19 +0000

    dev-metro-ian

---
+++ src/components/units/signup/register/register.style.ts
@@ -0,0 +1,19 @@
+import styled from '@emotion/native';
+
+export const Wrapper = styled.ScrollView`
+  padding: 35px;
+`;
+
+export const SubmitWrapper = styled.View`
+  background-color: blue;
+  /* margin-bottom: 50px; */
+  /* margin-top: 150px; */
+`;
+
+export const GreenButton = styled.TextInput`
+  text-align: center;
+`;
+
+export const InputBasic = styled.TextInput`
+  width: 100px;
+`;
