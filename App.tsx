9b7ebcc6f3a9d16b1efe8f7cc6ec0987215d056e
ekiankim92 <eunkukkim1992@gmail.com>
2022-01-03 03:47:44 +0000

    dev-login-ian

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Navigation from './pages/navigation';
 import 'react-native-gesture-handler';
+import LoginPage from './pages/screens/signup/login/index';
 
 const App = () => {
   return (
     <>
       <SafeAreaView />
+      <LoginPage />
       <Navigation />
     </>
   );
