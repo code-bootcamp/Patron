4f7977d788c259cb2c5f494e5e4b36cc8d21320a
ekiankim92 <eunkukkim1992@gmail.com>
2022-01-03 03:03:53 +0000

    dev-login-update-ian

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
+// prettier-ignore
 import {AppRegistry} from 'react-native';
 import App from './App';
+// prettier-ignore
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
