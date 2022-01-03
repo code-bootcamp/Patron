ad30275cf4593e9107e4d048701a623b3a4bd0e9
ekiankim92 <eunkukkim1992@gmail.com>
2022-01-03 07:19:22 +0000

    dev-client-ian

--- App.tsx
+++ App.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Navigation from './pages/navigation';
 import 'react-native-gesture-handler';
-import HomeCampaign from './src/components/units/home/campaign/HomeCampaign.container';
+import Register from './src/components/units/signup/register/register.container';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const App = () => {
+  const client = new ApolloClient({
+    uri: 'http://backend04-team.codebootcamp.co.kr/team03',
+    cache: new InMemoryCache(),
+  });
+
   return (
     <>
-      <HomeCampaign />
-      <SafeAreaView />
-      <Navigation />
+      <ApolloProvider client={client}>
+        <SafeAreaView />
+        <Register />
+        <Navigation />
+      </ApolloProvider>
     </>
   );
 };
