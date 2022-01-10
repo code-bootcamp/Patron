98c1aa2c0d7eb31d6b063337aaa39cb5dc255580
ekiankim92 <eunkukkim1992@gmail.com>
2022-01-10 07:40:02 +0000

    dev-login-update-ian

--- pages/screens/signup/register/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Register from '../../../../src/components/units/signup/register/register.container';
-
-const RegisterPage = () => {
-  return <Register />;
-};
-export default RegisterPage;
