940b4c75619008eda5370edcfba9eeb67c8b2144
ekiankim92 <eunkukkim1992@gmail.com>
2022-01-14 05:48:45 +0000

    dev-login-final-ian

--- src/components/units/signup/register/register.presenter.tsx
+++ src/components/units/signup/register/register.presenter.tsx
@@ -21,7 +21,7 @@ const RegisterUI = (props) => {
             <S.Email>이메일 :</S.Email>
             <InputBasic onChange={props.onChangeEmail} value={props.email} />
           </S.Email_Wrapper>
-          <R.Text>{props.emailError}</R.Text>
+          <S.Error_Text>{props.emailError}</S.Error_Text>
           <S.Password_Wrapper>
             <S.Password>비밀번호 :</S.Password>
             <InputBasic
@@ -30,22 +30,51 @@ const RegisterUI = (props) => {
               secureTextEntry={true}
             />
           </S.Password_Wrapper>
-          <R.Text>{props.passwordError}</R.Text>
+          <S.Error_Text>{props.passwordError}</S.Error_Text>
           <S.PasswordConfirm_Wrapper>
             <S.Password_Confirm>비밀번호 재확인 :</S.Password_Confirm>
             <InputBasic onChange={props.onChangePassConfirm} secureTextEntry={true} />
           </S.PasswordConfirm_Wrapper>
-          <R.Text>{props.passwordConfirmError}</R.Text>
+          <S.Error_Text>{props.passwordConfirmError}</S.Error_Text>
           <S.Name_Wrapper>
-            <R.Text>닉네임 :</R.Text>
+            <S.Name>닉네임 :</S.Name>
             <InputBasic onChange={props.onChangeName} value={props.name} />
           </S.Name_Wrapper>
-          <R.Text>{props.nameError}</R.Text>
+          <S.Error_Text>{props.nameError}</S.Error_Text>
+          <S.Email_Auth>
+            <S.Email_Authentication>이메일 인증 :</S.Email_Authentication>
+            <InputBasic onChange={props.onChangeEmailConfirm} />
+          </S.Email_Auth>
+          <S.Auth>
+            <InputBasic
+              width="65%"
+              height="95%"
+              placeholder="인증번호"
+              onChange={props.onChangeTempNumber}
+            />
+            {!props.isAuth ? (
+              <GreenButton
+                text="인증받기"
+                onPressBtn={props.onPressAuth}
+                width="108px"
+                height="48px"
+              />
+            ) : (
+              <GreenButton
+                text="인증하기"
+                onPressBtn={props.onPressAuth}
+                width="108px"
+                height="48px"
+              />
+            )}
+          </S.Auth>
+          <R.Text>{props.tempNumber}</R.Text>
           <S.Submit_Wrapper>
             <GreenButton
               text="회원가입"
               fontSize="20px;"
               width="100%"
+              height="50px"
               onPressBtn={props.onPressSubmit}
             />
           </S.Submit_Wrapper>
